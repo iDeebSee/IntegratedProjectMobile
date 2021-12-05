fa42ddd54661738c266f0d3fbd9b611d5a79ef20
Abdelmajid Amiri <58814178+iDeebSee@users.noreply.github.com>
2021-12-05 17:27:03 +0100

    fixed lon and lat

--- app/src/main/java/be/ap/edu/integratedprojectmobile/StudentLoginActivity.kt
+++ app/src/main/java/be/ap/edu/integratedprojectmobile/StudentLoginActivity.kt
@@ -1,11 +1,23 @@
 package be.ap.edu.integratedprojectmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class StudentLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_login)
+
+        val txtSnummer = findViewById<TextView>(R.id.txtSnummer)
+        val btnLogin = findViewById<Button>(R.id.btnStudentLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, StudentsActivity::class.java)
+            intent.putExtra("snummer", txtSnummer.text.toString())
+            startActivity(intent)
+        }
+
     }
 }
